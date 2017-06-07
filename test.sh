dcd45cdd87e51b1d2c7a13ec6767d356d3211fca
Daniel Miranda <danielkza2@gmail.com>
2017-06-07 03:38:43 -0300

    tests: improve test output, fail on style issues

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-flake8 shelver || :
-coverage erase && \
-coverage run --source shelver -m py.test && \
+set -e
+flake8 shelver
+coverage erase
+coverage run --source shelver -m py.test
 coverage report --include='shelver/**' --omit='shelver/test/**'
