7a6f4665c54c2ef39bade053d70d2f0e75cb9d83
Daniel Miranda <danielkza2@gmail.com>
2017-06-07 06:39:14 -0300

    tests: make timeouts more lenient in async tests

--- shelver/test/test_util_async.py
+++ shelver/test/test_util_async.py
@@ -33,7 +33,7 @@ def test_async_base_delay(event_loop):
     # Run a sleep in the background with delay, get the time in a second
     # coroutine and compare with the time after the sleep to check for blocking
     def get_time():
-        time.sleep(0.1)
+        time.sleep(0.2)
         return event_loop.time()
 
     f = ensure_future(async_obj.delay(get_time))
@@ -56,12 +56,12 @@ def test_async_loop_supervisor_init(event_loop):
     assert supervisor.timeout == 10
 
 
-def _count_cancellations(timeout, times=1):
+def _count_cancellations(timeout, times=1, loop=None):
     cancel_count = 0
 
     for _ in range(times):
         try:
-            yield from asyncio.sleep(timeout)
+            yield from asyncio.sleep(timeout, loop=loop)
         except asyncio.CancelledError:
             cancel_count += 1
 
@@ -77,10 +77,10 @@ def _interrupt(timeout, times=1):
 
 
 @pytest.mark.boxed
-def test_async_loop_supervisor_supervise(supervisor):
+def test_async_loop_supervisor_supervise(event_loop, supervisor):
     supervisor.timeout = 1
     cancellations = supervisor.supervise(
-        _count_cancellations(0.1, times=1))
+        _count_cancellations(0.2, times=1, loop=event_loop))
 
     assert not supervisor.loop.is_running()
     assert cancellations == 0
@@ -88,18 +88,18 @@ def test_async_loop_supervisor_supervise(supervisor):
 
 
 @pytest.mark.boxed
-def test_async_loop_supervisor_interrupt(supervisor):
+def test_async_loop_supervisor_interrupt(event_loop, supervisor):
     # Set up the supervisor with a short timeout. Also send a signal a short
     # time after starting. The supervisor should pick up the first signal, send
     # the first cancellation, then afterwards send the second cancellation when
     # the timeout expires.
 
-    t = threading.Timer(0.1, _interrupt, (0.1, 1))
+    t = threading.Timer(0.2, _interrupt, (0.2, 1))
     t.start()
-    supervisor.timeout = 0.1
+    supervisor.timeout = 0.2
 
     cancellations = supervisor.supervise(
-        _count_cancellations(0.2, times=2))
+        _count_cancellations(0.5, times=2, loop=event_loop))
 
     assert not supervisor.loop.is_running()
     assert cancellations == 2
@@ -107,16 +107,16 @@ def test_async_loop_supervisor_interrupt(supervisor):
 
 
 @pytest.mark.boxed
-def test_async_loop_supervisor_double_interrupt(supervisor):
+def test_async_loop_supervisor_double_interrupt(event_loop, supervisor):
     # Same as above, but send signals twice to check if the second one triggers
     # a second cancellation, just as a timeout would.
 
-    t = threading.Timer(0.1, _interrupt, (0.1, 2))
+    t = threading.Timer(0.2, _interrupt, (0.2, 2))
     t.start()
     supervisor.timeout = 2
 
     cancellations = supervisor.supervise(
-        _count_cancellations(0.2, times=2))
+        _count_cancellations(0.5, times=2, loop=event_loop))
 
     assert not supervisor.loop.is_running()
     assert cancellations == 2
